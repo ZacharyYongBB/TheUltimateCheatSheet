da581a6a41cc2b272cab3e2cf813c20491d702d5
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-09-25 14:46:27 +0800

    Initial Commit

---
+++ TheUltimateCheatSheet/TheUltimateCheatSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheUltimateCheatSheetApp.swift
+//  TheUltimateCheatSheet
+//
+//  Created by Zachary on 25/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TheUltimateCheatSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
